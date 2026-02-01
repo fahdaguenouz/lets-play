5f22cbecb137e37bfb2b2640190cfcb088df6b85
fahdaguenouz <faguenouz@gmail.com>
2026-02-01 18:56:10 +0100

    error in db connection

--- mongo-init/01-create-app-user.js
+++ mongo-init/01-create-app-user.js
@@ -1,10 +1,19 @@
-db = db.getSiblingDB("lets_play");
+// This script runs ONLY on the first initialization of the Mongo volume.
+// If you already have data in the volume, it will NOT re-run (important).
+
+const dbName = process.env.MONGO_DB || "lets_play";
+const appUser = process.env.MONGO_APP_USERNAME || "letsplay";
+const appPass = process.env.MONGO_APP_PASSWORD || "letsplaypass";
+
+db = db.getSiblingDB(dbName);
 
 db.createUser({
-  user: "letsplay",
-  pwd: "letsplaypass",
+  user: appUser,
+  pwd: appPass,
   roles: [
-    { role: "readWrite", db: "lets_play" },
-    { role: "dbAdmin",  db: "lets_play" }
-  ]
+    { role: "readWrite", db: dbName },
+    { role: "dbAdmin", db: dbName },
+  ],
 });
+
+print(`✅ Created user '${appUser}' on db '${dbName}'`);
